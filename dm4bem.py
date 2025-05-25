26e06c43cd6f9e46da7960575daa8976d345a1cb
Maxime_Zinet <maximezinet@gmail.com>
2025-05-25 18:45:00 +0200

    Update dm4bem.py

--- dm4bem.py
+++ dm4bem.py
@@ -1393,8 +1393,8 @@ def tc2ss(TC):
     u = pd.concat([b, f0, fc])
 
     # DAE (Differential Algebraic Equations), Eq.(13)
-    K = -A.T @ G @ A
-    Kb = A.T @ G
+    K = -A.values.T @ np.diag(G.values) @ A.values
+    Kb = A.values.T @ np.diag(G)
 
     # Partition K and Kb based on values of diagonal of C, Eq.(14)
     K00 = K.loc[C_diag == 0, C_diag == 0]
